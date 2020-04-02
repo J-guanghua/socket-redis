059c6470b1d225d581075c48d26603576add5043
J-guanghua <hua.163com@qq.com>
2020-04-02 15:03:50 +0800

    组件调用 和 配置

--- guanghua.php
+++ guanghua.php
@@ -5,15 +5,15 @@ namespace guanghua\redis;
 
 class guanghua
 {	
-	const REDIS_NAME = 'redis'; // 共享组件 redis
-	const CACHE_NAME = 'cache'; //共享组件 cache
-    const SESSION_NAME = 'session'; //共享组件 session
-	
-	//redis连接配置
-	const HOST_LINK = [
-		'hostname' => '121.37.3.163', //连接主机
-		'port'     => 6379, //连接端口
-		'database' => 0 //连接数据库
+    //队列配置项
+    public static $redisArray = [
+        'redis'=>[
+            'hostname' => '121.37.3.163', //连接主机
+            'port'     => 6379, //连接端口
+            'database' => 0 //连接数据库
+        ],
+        'cache' => [],
+        'session' => [],
     ];
 
     //数组共享组件实例的id索引
@@ -22,29 +22,33 @@ class guanghua
     //当前redis实例
     public static function redis()
     {
-    	return self::ensure(guanghua::REDIS_NAME,RedisConn::class,guanghua::HOST_LINK);
+    	return self::ensure('redis',RedisConn::class);
     }
     
     //当前redis缓存实例
     public static function cache()
     {
-    	return self::ensure(guanghua::CACHE_NAME,RedisCache::class,[]);
+    	return self::ensure('cache',RedisCache::class);
     }
 
     //当前session缓存实例
     public static function session()
     {
-        return self::ensure(guanghua::SESSION_NAME,RedisSession::class,[]);
+        return self::ensure('session',RedisSession::class);
     }
 
     //得到一个实例化的类对象 并注册到共享组件
-    public static function ensure($id ,$class ,$array = []){
+    public static function ensure($id ,$class){
        
         if (isset(self::$_components[$id])) {
             
             return self::$_components[$id];
         }
-        return self::$_components[$id] = new $class($array);
+        if (isset(static::$redisArray[$id])) {
+
+            return self::$_components[$id] = new $class(static::$redisArray[$id]);
+        }
+        throw new Exception($id .' NOT : ' . get_class($this) . '::' . $class);
     }
 }
 ?>
